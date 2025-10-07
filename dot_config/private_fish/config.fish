e1f35f10716ab15b0fb25e053bf4cd6775b5d1c7
Muhammad Afif Ramadhan <ramadhanafif@gmail.com>
2025-10-07 16:52:43 +0200

    add waybar, add direnv

--- dot_config/private_fish/config.fish
+++ dot_config/private_fish/config.fish
@@ -3,6 +3,8 @@ if status is-interactive
     zoxide init --cmd cd fish | source
 end
 
+direnv hook fish | source
+
 # bun
 set --export BUN_INSTALL "$HOME/.bun"
 set --export PATH $BUN_INSTALL/bin $PATH
@@ -11,13 +13,14 @@ set --export PATH $BUN_INSTALL/bin $PATH
 set -gx PATH "$PATH:/home/ramad/.local/share/coursier/bin"
 # <<< coursier install directory <<<
 
-
 # BEGIN opam configuration
 # This is useful if you're using opam as it adds:
 #   - the correct directories to the PATH
 #   - auto-completion for the opam binary
 # This section can be safely removed at any time if needed.
-test -r '/home/ramad/.opam/opam-init/init.fish' && source '/home/ramad/.opam/opam-init/init.fish' > /dev/null 2> /dev/null; or true
+test -r '/home/ramad/.opam/opam-init/init.fish' && source '/home/ramad/.opam/opam-init/init.fish' >/dev/null 2>/dev/null; or true
 # END opam configuration
 
-set -q GHCUP_INSTALL_BASE_PREFIX[1]; or set GHCUP_INSTALL_BASE_PREFIX $HOME ; set -gx PATH $HOME/.cabal/bin /home/ramad/.ghcup/bin $PATH # ghcup-env
+set -q GHCUP_INSTALL_BASE_PREFIX[1]; or set GHCUP_INSTALL_BASE_PREFIX $HOME
+set -gx PATH $HOME/.cabal/bin /home/ramad/.ghcup/bin $PATH # ghcup-env
+
